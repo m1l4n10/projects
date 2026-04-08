6caa66b8370187f85b74a6f779b99c769dd51d53
milan <milan.duettra@gmail.com>
2026-04-08 20:41:36 +0200

    new file:   quitly/index.html 	new file:   quitly/manifest.json 	new file:   quitly/sw.js

--- The Open Road Administrator-Tools/assets/js/password.js
+++
@@ -1,9 +0,0 @@
-function handleLogin() {
-            const user = document.getElementById('username').value;
-            const pass = document.getElementById('password').value;
-            if (!user || !pass) {
-                alert('Bitte Benutzername und Passwort eingeben.');
-                return;
-            }
-            alert('Anmeldung für: ' + user);
-        }
